a0fd16e7234a4ae0c98e566a87951d533ff978e3
HYPER2208 <lcs2019014@iiitl.ac.in>
2023-05-03 19:23:21 +0530

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 
+import Lists from "./lists";
+import ProductList from '../src/components/ProductList'
+import Navbar from '../src/components/Navbar'
 export default function Home() {
   return (
   <div>
-    Welcome to ecommerce app!
+    {/* Welcome to ecommerce app! */}
+    {/* <Lists /> */}
+    <Navbar/>
+    <ProductList />
   </div>
   )
 }
